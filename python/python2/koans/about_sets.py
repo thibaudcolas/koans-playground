35be49f0af6962a1dc724cc4a752d93c15f415e3
Thibaud Colas <thibaudcolas@gmail.com>
2014-08-16 09:29:14 +0200

    sets secund part

--- python/python2/koans/about_sets.py
+++ python/python2/koans/about_sets.py
@@ -25,19 +25,19 @@ class AboutSets(Koan):
         scotsmen = set(['MacLeod', 'Wallace', 'Willie'])
         warriors = set(['MacLeod', 'Wallace', 'Leonidas'])
 
-        self.assertEqual(__, scotsmen - warriors)
-        self.assertEqual(__, scotsmen | warriors)
-        self.assertEqual(__, scotsmen & warriors)
-        self.assertEqual(__, scotsmen ^ warriors)
+        self.assertEqual(set(['Willie']), scotsmen - warriors)
+        self.assertEqual(set(['MacLeod', 'Wallace', 'Leonidas', 'Willie']), scotsmen | warriors)
+        self.assertEqual(set(['MacLeod', 'Wallace']), scotsmen & warriors)
+        self.assertEqual(set(['Willie', 'Leonidas']), scotsmen ^ warriors)
 
     # ------------------------------------------------------------------
 
     def test_we_can_query_set_membership(self):
-        self.assertEqual(__, 127 in set([127, 0, 0, 1]))
-        self.assertEqual(__, 'cow' not in set('apocalypse now'))
+        self.assertEqual(True, 127 in set([127, 0, 0, 1]))
+        self.assertEqual(True, 'cow' not in set('apocalypse now'))
 
     def test_we_can_compare_subsets(self):
-        self.assertEqual(__, set('cake') <= set('cherry cake'))
-        self.assertEqual(__, set('cake').issubset(set('cherry cake')))
+        self.assertEqual(True, set('cake') <= set('cherry cake'))
+        self.assertEqual(True, set('cake').issubset(set('cherry cake')))
 
-        self.assertEqual(__, set('cake') > set('pie'))
+        self.assertEqual(False, set('cake') > set('pie'))
