f2f9dfe4cca2c10b21ef97f837b4ef1c97396f9a
Thibaud Colas <thibaudcolas@gmail.com>
2014-08-26 23:51:49 +0200

    roll roll roll

--- python/python2/koans/about_scoring_project.py
+++ python/python2/koans/about_scoring_project.py
@@ -34,8 +34,9 @@ from runner.koan import *
 # Your goal is to write the score method.
 
 def score(dice):
-    # You need to write this method
-    pass
+    result = 0
+
+    return result
 
 
 class AboutScoringProject(Koan):
