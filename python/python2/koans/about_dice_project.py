64037d4da4ef0e6fef1308cb3540ec120f5766cc
Thibaud Colas <thibaudcolas@gmail.com>
2014-10-03 21:29:03 +0200

    dice project: one liner?

--- python/python2/koans/about_dice_project.py
+++ python/python2/koans/about_dice_project.py
@@ -15,9 +15,7 @@ class DiceSet(object):
         return self._values
 
     def roll(self, n):
-        # Needs implementing!
-        # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = [random.randint(1, 6) for i in range(0, n)]
 
 
 class AboutDiceProject(Koan):
