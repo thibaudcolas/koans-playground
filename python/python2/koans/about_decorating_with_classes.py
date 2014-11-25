c4a66fa0dd9cecd5e5f1cf57aaf713a2517f3cc7
Thibaud Colas <thibaudcolas@gmail.com>
2014-11-25 13:41:01 +0100

    Partial application ftw

--- python/python2/koans/about_decorating_with_classes.py
+++ python/python2/koans/about_decorating_with_classes.py
@@ -20,21 +20,21 @@ class AboutDecoratingWithClasses(Koan):
         """
         max = functools.partial(self.maximum)
 
-        self.assertEqual(__, max(7, 23))
-        self.assertEqual(__, max(10, -10))
+        self.assertEqual(23, max(7, 23))
+        self.assertEqual(10, max(10, -10))
 
     def test_partial_that_wrappers_first_arg(self):
         max0 = functools.partial(self.maximum, 0)
 
-        self.assertEqual(__, max0(-4))
-        self.assertEqual(__, max0(5))
+        self.assertEqual(0, max0(-4))
+        self.assertEqual(5, max0(5))
 
     def test_partial_that_wrappers_all_args(self):
         always99 = functools.partial(self.maximum, 99, 20)
         always20 = functools.partial(self.maximum, 9, 20)
 
-        self.assertEqual(__, always99())
-        self.assertEqual(__, always20())
+        self.assertEqual(99, always99())
+        self.assertEqual(20, always20())
 
     # ------------------------------------------------------------------
 
