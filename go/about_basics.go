99bc92d426a4804a4bdc5e1de35192be74564703
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-15 23:48:02 +1300

    Enlightneded about basics, default values of each type

--- go/about_basics.go
+++ go/about_basics.go
@@ -12,20 +12,20 @@ func aboutBasics() {
 	assert(5^2 == 7)
 
 	var x int
-	assert(x == __int__) // zero values are valued in Go
+	assert(x == 0) // zero values are valued in Go
 
 	var f float32
-	assert(f == __float32__) // for types of all types
+	assert(f == 0) // for types of all types
 
 	var s string
-	assert(s == __string__) // both typical or atypical types
+	assert(s == "") // both typical or atypical types
 
 	var c struct {
 		x int
 		f float32
 		s string
 	}
-	assert(c.x == __int__)     // and types within composite types
-	assert(c.f == __float32__) // which match the other types
-	assert(c.s == __string__)  // in a typical way
+	assert(c.x == 0)     // and types within composite types
+	assert(c.f == 0) // which match the other types
+	assert(c.s == "")  // in a typical way
 }
