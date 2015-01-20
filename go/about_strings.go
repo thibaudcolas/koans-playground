498b4a14125dbc72aee3511f867848d3d46e3d04
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-20 23:50:12 +1300

    Finish about strings

--- go/about_strings.go
+++ go/about_strings.go
@@ -17,14 +17,14 @@ func aboutStrings() {
 	assert("smith" < "sxyzfs")  // i suppose maybe this could be useful.. someday
 
 	bytes := []byte{'a', 'b', 'c'}
-	assert(string(bytes) == __string__) // strings can be created from byte-slices
+	assert(string(bytes) == "abc") // strings can be created from byte-slices
 
 	bytes[0] = 'z'
-	assert(string(bytes) == __string__) // byte-slices can be mutated, although strings cannot
+	assert(string(bytes) == "zbc") // byte-slices can be mutated, although strings cannot
 
-	assert(fmt.Sprintf("hello %s", __string__) == "hello world") // our old friend sprintf returns
-	assert(fmt.Sprintf("hello \"%s\"", "world") == __string__)   // quoting is familiar
-	assert(fmt.Sprintf("hello %q", "world") == __string__)       // although it can be done more easily
+	assert(fmt.Sprintf("hello %s", "world") == "hello world") // our old friend sprintf returns
+	assert(fmt.Sprintf("hello \"%s\"", "world") == "hello \"world\"")   // quoting is familiar
+	assert(fmt.Sprintf("hello %q", "world") == "hello \"world\"")       // although it can be done more easily
 
-	assert(fmt.Sprintf("your balance: %d and %0.2f", 3, 4.5589) == __string__) // "the root of all evil" is actually a misquotation, by the way
+	assert(fmt.Sprintf("your balance: %d and %0.2f", 3, 4.5589) == "your balance: 3 and 4.56") // "the root of all evil" is actually a misquotation, by the way
 }
