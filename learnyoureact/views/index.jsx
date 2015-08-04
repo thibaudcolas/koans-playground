4fa05f116852b3abde2f32b3b1dbdd812231e3df
Thibaud Colas <thibaudcolas@gmail.com>
2015-08-04 21:56:36 +1200

    Second exercise solution

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -4,8 +4,30 @@ var TodoBox = React.createClass({
   render: function() {
     return (
       <div className="todoBox">
-        Hello, world!
+        <h1>Todos</h1>
+        <TodoList />
+        <TodoForm />
       </div>
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+    return (
+        <div className="todoList">
+            I am a TodoList.
+        </div>
+    );
+  }
+});
+
+var TodoForm = React.createClass({
+  render: function() {
+    return (
+        <div className="todoForm">
+            I am a TodoForm.
+        </div>
     );
   }
 });
