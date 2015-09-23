fbafcb168293554fe61166ab916de08ad6d10786
Thibaud Colas <thibaudcolas@gmail.com>
2015-09-23 21:33:04 +1200

    Add third answer to lololodash

---
+++ lololodash/ineverycase.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+
+export default function(collection) {
+    return _.forEach(collection, (item, key) => {
+        let size;
+
+        if (item.population > 1) {
+            size = 'big';
+        } else if (item.population > 0.5) {
+            size = 'med';
+        } else {
+            size = 'small';
+        }
+
+        item.size = size;
+    });
+};
