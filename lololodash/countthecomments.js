dc7e14031a2320aa82cf0f352d2bc15eb3929bcf
Thibaud Colas <thibaudcolas@gmail.com>
2015-09-23 22:58:17 +1200

    Count the comments

--- lololodash/countthecomments.js
+++ lololodash/countthecomments.js
@@ -2,13 +2,13 @@ import _ from 'lodash'
 
 export default function(collection) {
     return _.chain(collection)
-            .groupBy('username')
-            .map((comments, username) => {
-                return {
-                    username: username,
-                    comment_count: _.size(comments),
-                }
-            })
-            .sortBy('comment_count')
-            .reverse()
+        .groupBy('username')
+        .map((comments, username) => {
+            return {
+                username: username,
+                comment_count: _.size(comments),
+            }
+        })
+        .sortBy('comment_count')
+        .reverse()
 }
---
+++ lololodash/overview.js
@@ -0,0 +1,17 @@
+import _ from 'lodash'
+
+export default function (collection) {
+    return _.chain(collection)
+        .groupBy('article')
+        .map((articles) => {
+            return {
+                article: articles[0].article,
+                total_orders: _.chain(articles)
+                    .pluck('quantity')
+                    .reduce((sum, num) => sum + num, 0)
+                    .value()
+            }
+        })
+        .value()
+        .reverse()
+}
