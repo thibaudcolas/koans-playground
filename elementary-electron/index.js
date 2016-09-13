20802594c52ab15b278973e8c064d142457b789a
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-13 23:05:38 +0300

    Add comments for code sample

--- elementary-electron/index.js
+++ elementary-electron/index.js
@@ -4,14 +4,18 @@ var image = require('lightning-image-poly');
 var picture = require('cat-picture');
 var src = picture.src;
 
+// Remove the default cat picture.
 picture.remove();
 
+// Create the polygon editor, with the image.
 var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
 
 function save() {
+    // Electron magic to make a PDF?
     remote.getCurrentWindow().webContents.printToPDF({
         portrait: true,
     }, function(err, data) {
+        // Use node file system module to create a file.
         fs.writeFile('annotation.pdf', data, function(err) {
             if (err) {
                 window.alert('error generating pdf! ' + err.message);
