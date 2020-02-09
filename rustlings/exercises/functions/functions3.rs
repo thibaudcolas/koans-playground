28450949c1a633a15dfd7d12d3d65dcc21ad8fc6
Thibaud Colas <thibaudcolas@gmail.com>
2020-02-09 23:51:26 +0000

    Add more rustlings

--- rustlings/exercises/functions/functions3.rs
+++ rustlings/exercises/functions/functions3.rs
@@ -1,14 +1,12 @@
 // functions3.rs
 // Make me compile! Execute `rustlings hint functions3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+  call_me(30);
 }
 
 fn call_me(num: i32) {
-    for i in 0..num {
-        println!("Ring! Call number {}", i + 1);
-    }
+  for i in 0..num {
+    println!("Ring! Call number {}", i + 1);
+  }
 }
